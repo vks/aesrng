392468d87ddb9e23f6aeba9386d3aad123a6e77c
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-08-13 14:51:32 +0200

    Benchmark vs. xoshiro

--- benches/rngs.rs
+++ benches/rngs.rs
@@ -1,6 +1,6 @@
 extern crate aesrng;
 extern crate rand;
-extern crate xoroshiro;
+extern crate xoshiro;
 
 #[macro_use]
 extern crate criterion;
@@ -28,22 +28,20 @@ fn fill(c: &mut Criterion) {
 
         Fun::new("aescore", move |b, _| b.iter(|| rng.fill(&mut buf)))
     };
-    /*
-    let fill_xoroshiro =
+    let fill_xoshiro =
         {
-            let mut rng = xoroshiro::rng::XoroShiro128::new_unseeded();
+            let mut rng = xoshiro::Xoshiro128StarStar::from_seed_u64(1);
             let mut buf = vec![0; BUF_SIZE];
 
-            Fun::new("xoroshiro", move |b, _| b.iter(|| rng.fill_bytes(&mut buf)))
+            Fun::new("xoshiro", move |b, _| b.iter(|| rng.fill_bytes(&mut buf)))
         };
-    */
     let fill_std = {
         let mut rng = rand::StdRng::from_entropy();
         let mut buf = vec![0; BUF_SIZE];
 
         Fun::new("std", move |b, _| b.iter(|| rng.fill_bytes(&mut buf)))
     };
-    c.bench_functions("fill", vec![fill_aes, fill_aescore, /*fill_xoroshiro,*/ fill_std], ());
+    c.bench_functions("fill", vec![fill_aes, fill_aescore, fill_xoshiro, fill_std], ());
 }
 
 fn next_u64(c: &mut Criterion) {
@@ -54,18 +52,16 @@ fn next_u64(c: &mut Criterion) {
         ]);
         Fun::new("aes", move |b, _| b.iter(|| rng.next_u64()))
     };
-    /*
-    let next_xoroshiro =
+    let next_xoshiro =
         {
-            let mut rng = xoroshiro::rng::XoroShiro128::new_unseeded();
-            Fun::new("xoroshiro", move |b, _| b.iter(|| rng.next_u64()))
+            let mut rng = xoshiro::Xoshiro128StarStar::from_seed_u64(1);
+            Fun::new("xoshiro", move |b, _| b.iter(|| rng.next_u64()))
         };
-    */
     let next_std = {
         let mut rng = rand::StdRng::from_entropy();
         Fun::new("std", move |b, _| b.iter(|| rng.next_u64()))
     };
-    c.bench_functions("next_u64", vec![next_aes, /*next_xoroshiro,*/ next_std], ());
+    c.bench_functions("next_u64", vec![next_aes, next_xoshiro, next_std], ());
 }
 
 fn new(c: &mut Criterion) {
@@ -77,14 +73,12 @@ fn new(c: &mut Criterion) {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
             11, 12, 13, 14, 15,
         ])));
-    /*
-    let new_xoroshiro = Fun::new("xoroshiro", |b, _| b.iter(|| xoroshiro::rng::XoroShiro128::new_unseeded()));
-    */
+    let new_xoshiro = Fun::new("xoshiro", |b, _| b.iter(|| xoshiro::Xoshiro128StarStar::from_seed_u64(1)));
     let new_std = Fun::new("std", |b, _| b.iter(|| rand::StdRng::from_seed([
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
             11, 12, 13, 14, 15,
         ])));
-    c.bench_functions("new", vec![new_aes, new_aescore, /*new_xoroshiro,*/ new_std], ());
+    c.bench_functions("new", vec![new_aes, new_aescore, new_xoshiro, new_std], ());
 }
 
 criterion_group!(benches, fill, next_u64, new);
